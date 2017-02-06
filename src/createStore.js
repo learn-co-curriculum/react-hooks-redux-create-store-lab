99b7908fd3051e45a32c125c93bef21b00ea0f3d
Jeff Katz <jeffreyerickatz@gmail.com>
2017-02-06 10:22:00 -0500

    update createStore

--- src/createStore.js
+++ src/createStore.js
@@ -4,20 +4,7 @@ export default function createStore(reducer){
 
 // solution
 export default function createStore(reducer){
-  let state;
-
-  function dispatch(action){
-    state = reducer(state, action)
-    render()
-  }
-
-  function getState(){
-    return state;
-  }
-  dispatch({type: '@@INIT'})
-  return {dispatch: dispatch, getState: getState}
 }
 
 function render(){
-  let container = document.getElementById('container')
 }
