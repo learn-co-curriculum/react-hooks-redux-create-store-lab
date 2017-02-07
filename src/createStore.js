6d5f820ca5c4ad8a255a9bc3582a0f13f7ebcb42
Luke Ghenco <lukeghenco@gmail.com>
2017-02-07 06:21:48 -0500

    add arbitrary code to render method

--- src/createStore.js
+++ src/createStore.js
@@ -1,6 +1,7 @@
-export default function createStore(reducer){
-
+export default function createStore(reducer) {
+  // add your code here
 }
 
-function render(){
+function render() {
+  const container = document.getElementById('container');
 }
