43cba5c2aac253b21d5a64cfa6bf119de0888e43
Luke Ghenco <lukeghenco@gmail.com>
2017-02-07 06:22:33 -0500

    update creatStore solution syntax

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,21 @@
 export default function createStore(reducer) {
-  // add your code here
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action)
+    render()
+  }
+
+  function getState() {
+    return state;
+  }
+
+  dispatch({ type: '@@INIT' })
+
+  return {
+    dispatch,
+    getState
+  }
 }
 
 function render() {
