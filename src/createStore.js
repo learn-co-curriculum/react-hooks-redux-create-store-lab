8a308f9db577075ec6ecd6c8e9cc4dfa37b0693f
maxwell <github email address>
2018-07-23 09:55:08 -0400

    reviewed, updated

--- src/createStore.js
+++ src/createStore.js
@@ -1,21 +1,5 @@
 export default function createStore(reducer) {
-  let state;
-
-  function dispatch(action) {
-    state = reducer(state, action)
-    render()
-  }
-
-  function getState() {
-    return state;
-  }
-
-  dispatch({ type: '@@INIT' })
-
-  return {
-    dispatch,
-    getState
-  }
+  // add your code here
 }
 
 function render() {
