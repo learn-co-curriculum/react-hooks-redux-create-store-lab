d068db591686c0f10e08692ac86e137d49284922
maxwellbenton <maxwellbenton@gmail.com>
2019-10-17 18:05:01 -0400

    -complexity, +solution works in browser

--- test/index-test.js
+++ test/index-test.js
@@ -1,45 +1,27 @@
-import createStore  from '../src/createStore';
-import candyReducer from '../src/reducers/candyReducer';
-import countReducer from '../src/reducers/countReducer';
-import jsdom from 'jsdom';
-import sinon from 'sinon';
-
-describe('createStore', function() {
-  let candyStore, countStore;
+describe('createStore()', () => {
+  let candyStore;
 
   beforeEach(function(){
-    global.document = jsdom.jsdom('<html><body><div id="container"></div></body></html>');
     candyStore = createStore(candyReducer);
-    countStore = createStore(countReducer);
   })
 
   it('returns an object', function() {
-    expect(candyStore).toBeA(Object)
+    expect(candyStore).to.be.an("object")
   })
 
-  describe('getState', function(){
+  describe('getState method', function(){
     it('returns the default state based on the reducer for the candyStore', function(){
-      expect(candyStore.getState()).toEqual([]);
-    })
-
-    it('returns the default state based on the reducer for the countStore', function(){
-      expect(countStore.getState()).toBeA('number')
-      expect(countStore.getState()).toEqual(0)
+      candyStore.dispatch({type: '@@INIT'})
+      expect(candyStore.getState()).to.eql([]);
     })
   })
 
-  describe('dispatch', function(){
+  describe('dispatch method', function(){
 
-    it('can dispatch actions to update the state', function(){
-      countStore.dispatch({type: 'INCREMENT_COUNT'});
-      expect(countStore.getState()).toEqual(1);
-
-    })
-
-    it('can dispatch actions with data associated', function(){
-      candyStore.dispatch({type: 'ADD_CANDY', candy: {name: "Jelly Beans"}});
+    it('can dispatch actions with data associated to update the state', function(){
+      candyStore.dispatch({type: 'ADD_CANDY', candy: "Jelly Beans"});
       let todos = candyStore.getState();
-      expect(todos).toEqual([{name: "Jelly Beans"}]);
+      expect(todos).to.eql(["Jelly Beans"]);
     })
   })
 })
